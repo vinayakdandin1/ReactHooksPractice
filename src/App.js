6b03c73432502becf9c1a49c0c2d6e709e63d5c1
Vinayak Dandin <vinayakdandin1@gmail.com>
2021-03-25 13:45:22 +0100

    select color added to widget

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Accordion from './components/Accordion'
+import Dropdown from './components/Dropdown'
 import Search from './components/Search'
 
 const items = [
@@ -18,11 +19,32 @@ const items = [
 
 ]
 
+const options = [
+
+    {
+        label: 'The color Red',
+        value: 'red'
+    },
+    {
+        label: 'The color Green',
+        value: 'green'
+    },
+    {
+        label: "A shade of Blue",
+        value: 'blue'
+    }
+
+];
+
 
 export default function App() {
+
+    const [selected, setSelected] = useState(options[0]);
+
+
     return (
         <div>
-            <Search />
+            <Dropdown selected={selected} onSelectedChange={setSelected} options={options} />
         </div>
     )
 }
