0557a01a339d1bb536a98e829c7a5f0348e3f528
Vinayak Dandin <vinayakdandin1@gmail.com>
2021-03-25 12:05:52 +0100

    Complete search widget

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,18 @@ import axios from 'axios'
 
 const Search = () => {
     const [term, setTerm] = useState("programming");
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -13,20 +24,24 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm
                 }
             })
-
             setResults(data.query.search);
+        };
 
-        }
+        search()
 
-        search()    
-            
-    }, [term]);
+    }, [debouncedTerm])
 
     const renderResults = results.map((result) => {
         return (<div key={result.pageid} className="item">
+            <div className="right floated content">
+                <a 
+                    className="ui button"
+                    href={`https://en.wikipedia.org?cyrud=${result.pageid}`}
+                >Go</a>
+            </div>
             <div className="content">
                 <div className="header">
                     {result.title}
