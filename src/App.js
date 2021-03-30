41167d70eaac5b11844531268d05fddac07e3e93
Vinayak Dandin <vinayakdandin1@gmail.com>
2021-03-30 14:16:19 +0200

    Complete translation widget

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import Accordion from './components/Accordion'
 import Dropdown from './components/Dropdown'
 import Search from './components/Search'
+import Translate from './components/Translate'
 
 const items = [
     {
@@ -39,15 +40,9 @@ const options = [
 
 export default function App() {
 
-    const [selected, setSelected] = useState(options[0]);
-    const [showDropdown, setShowDropdown] = useState(true);
-
     return (
         <div>
-            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-            {   showDropdown ? 
-                <Dropdown selected={selected} onSelectedChange={setSelected} options={options} /> : null
-            }
+            <Translate />
         </div>
     )
 }
